2060d5c3621e469fcb1ed64c9412453c3de2b528
tedesco.luigi <tedesco.luigi@INF-1016>
2026-01-07 13:30:12 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulazione_partita_di_calcio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
